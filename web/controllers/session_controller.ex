90e9b0710d2904ade64ea85df18673bf49e4999d
peillis <enrique@doofinder.com>
2016-10-29 20:53:34 +0200

    auth

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -2,7 +2,7 @@ defmodule Comiditas.SessionController do
   use Comiditas.Web, :controller
 
   alias Comiditas.User
-  alias Comiditas.UserQuery
+  alias Comiditas.Auth
 
   plug :scrub_params, "user" when action in [:create]
 
@@ -10,16 +10,23 @@ defmodule Comiditas.SessionController do
     render conn, "new.html"
   end
 
-  def create(conn, params = %{}) do
-    conn
-    |> put_flash(:info, "Logged in.")
-    # |> Guardian.Plug.sign_in(verified_user) # verify your logged in resource
-    |> redirect(to: user_path(conn, :index))
+  def create(conn, %{"user" => %{"email" => email, "password" => password}}) do
+    case Auth.login_by_email_and_pass(email, password) do
+      {:ok, verified_user} ->
+        conn
+        |> put_flash(:info, "Logged in.")
+        |> Guardian.Plug.sign_in(verified_user) # verify your logged in resource
+        |> redirect(to: user_path(conn, :index))
+      {:error, error} ->
+        conn
+        |> put_flash(:error, "Invalid credentials")
+        |> render("new.html")
+    end
   end
 
-  def delete(conn, _params) do
+  def delete(conn, _) do
     Guardian.Plug.sign_out(conn)
     |> put_flash(:info, "Logged out successfully.")
-    |> redirect(to: "/")
+    |> redirect(to: session_path(conn, :new))
   end
 end
--- web/router.ex
+++ web/router.ex
@@ -13,11 +13,18 @@ defmodule Comiditas.Router do
     plug :accepts, ["json", "json-api"]
   end
 
+  pipeline :browser_auth do
+    plug Guardian.Plug.VerifySession
+    plug Guardian.Plug.LoadResource
+    plug Guardian.Plug.EnsureAuthenticated, handler: Comiditas.Auth
+  end
+
   scope "/", Comiditas do
-    pipe_through :browser # Use the default browser stack
+    pipe_through :browser
     resources "/sessions", SessionController, only: [:new, :create, :delete]
 
     scope "/admin" do
+      pipe_through :browser_auth
       get "/", PageController, :index
       resources "/groups", GroupController
       resources "/users", UserController
