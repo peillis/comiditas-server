4559f7a7d5254c9fb65d412da58c9387b82bc6c6
peillis <enrique@doofinder.com>
2019-12-05 11:39:15 +0100

    mix task to purge data from database

---
+++ lib/mix/tasks/comiditas.purge.ex
@@ -0,0 +1,15 @@
+defmodule Mix.Tasks.Comiditas.Purge do
+  use Mix.Task
+
+  import Ecto.Query
+  alias Comiditas.Mealdate
+  alias Comiditas.Repo
+
+  @shortdoc "Purge mealdate table of old data."
+  def run(_) do
+    date = Timex.today() |> Timex.shift(months: -3)
+    Ecto.Migrator.with_repo(Repo, fn repo ->
+      repo.delete_all(from m in Mealdate, where: m.date < ^date)
+    end)
+  end
+end
