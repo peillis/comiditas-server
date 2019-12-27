7f139f8ff6b3a3d2d48c235de03d51bad592f499
peillis <enrique@doofinder.com>
2019-12-27 12:39:26 +0100

    add power_user field

--- lib/comiditas/admin/user.ex
+++ lib/comiditas/admin/user.ex
@@ -2,13 +2,12 @@ defmodule Comiditas.Admin.User do
   use Ecto.Schema
   import Ecto.Changeset
 
-  alias Argon2
-
   schema "users" do
     field :email, :string
     field :name, :string
     field :password_virtual, :string, virtual: true
     field :password, :string
+    field :power_user, :boolean
     belongs_to :group, Comiditas.Admin.Group
     has_many :templates, Comiditas.Template, on_delete: :delete_all
     has_many :mealdates, Comiditas.Mealdate, on_delete: :delete_all
@@ -19,7 +18,7 @@ defmodule Comiditas.Admin.User do
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:name, :email, :password, :group_id])
+    |> cast(attrs, [:name, :email, :password, :group_id, :power_user])
     |> validate_required([:name, :email, :password, :group_id])
     |> put_password_hash()
   end
