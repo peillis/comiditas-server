ed0dd7d48e04f55ce3ebb6b041f64b1f8a84c80d
peillis <enrique@doofinder.com>
2020-01-13 17:44:17 +0100

    fix for production server

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :comiditas, ComiditasWeb.Endpoint,
-  url: [scheme: "https", host: "comiendo.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "comiditas.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
