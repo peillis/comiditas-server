1a18d33db7277f03f670f134833a8b13a2214a4b
Enrique Martinez <enrique@doofinder.com>
2022-10-28 10:05:53 +0200

    more data purge

--- lib/mix/tasks/comiditas.purge.ex
+++ lib/mix/tasks/comiditas.purge.ex
@@ -7,7 +7,7 @@ defmodule Mix.Tasks.Comiditas.Purge do
 
   @shortdoc "Purge mealdate table of old data."
   def run(_) do
-    date = Timex.today() |> Timex.shift(months: -3)
+    date = Timex.today() |> Timex.shift(months: -2)
 
     Ecto.Migrator.with_repo(Repo, fn repo ->
       repo.delete_all(from m in Mealdate, where: m.date < ^date)
