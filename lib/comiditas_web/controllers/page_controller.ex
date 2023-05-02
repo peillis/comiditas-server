57bf903ec124a1570d0f7518d5e417dca00ba7b6
Enrique Martinez <enrique@doofinder.com>
2023-05-02 13:36:49 +0200

    add users

--- lib/comiditas_web/controllers/page_controller.ex
+++ lib/comiditas_web/controllers/page_controller.ex
@@ -13,23 +13,6 @@ defmodule ComiditasWeb.PageController do
     end
   end
 
-  #def list(conn, %{"uid" => uid}) do
-    #user = conn.assigns.current_user
-    #uid = String.to_integer(uid)
-    #live_render(conn, ComiditasWeb.Live.ListView, session: %{user: user, uid: uid})
-  #end
-
-  #def settings(conn, %{"uid" => uid}) do
-    #user = Guardian.Plug.current_resource(conn)
-    #uid = String.to_integer(uid)
-    #live_render(conn, ComiditasWeb.Live.SettingsView, session: %{user: user, uid: uid})
-  #end
-
-  #def totals(conn, _) do
-    #user = Guardian.Plug.current_resource(conn)
-    #live_render(conn, ComiditasWeb.Live.TotalsView, session: %{user: user})
-  #end
-
   def users(conn, _) do
     user = conn.assigns.current_user
     conn = assign(conn, :users, Comiditas.get_users(user.group_id))
@@ -37,28 +20,28 @@ defmodule ComiditasWeb.PageController do
   end
 
   def new(conn, _params) do
-    changeset = Admin.change_user(%User{})
+    changeset = Users.change_user(%User{})
     render(conn, "new.html", changeset: changeset)
   end
 
-  #def create(conn, %{"user" => user_params}) do
-    ## Add the group_id to the user
-    #power_user = Guardian.Plug.current_resource(conn)
-    #user_params = Map.put(user_params, "group_id", power_user.group_id)
+  def create(conn, %{"user" => user_params}) do
+    # Add the group_id to the user
+    power_user = conn.assigns.current_user
+    user_params = Map.put(user_params, "group_id", power_user.group_id)
 
-    #case Admin.create_user(user_params) do
-      #{:ok, _user} ->
-        #pid = Util.get_pid(power_user.group_id)
-        #GroupServer.refresh(pid)
+    case Users.create_user(user_params) do
+      {:ok, _user} ->
+        pid = Util.get_pid(power_user.group_id)
+        GroupServer.refresh(pid)
 
-        #conn
-        #|> put_flash(:info, "User created successfully.")
-        #|> redirect(to: Routes.page_path(conn, :users, uid: power_user.id))
+        conn
+        |> put_flash(:info, "User created successfully.")
+        |> redirect(to: Routes.page_path(conn, :users, uid: power_user.id))
 
-      #{:error, %Ecto.Changeset{} = changeset} ->
-        #render(conn, "new.html", changeset: changeset)
-    #end
-  #end
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
 
   def edit(conn, %{"uid" => uid}) do
     user = Users.get_user!(uid)
