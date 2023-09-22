1a4b8a1bde762b5812ac26d1b138a927c863f7f1
Enrique Martinez <enrique@doofinder.com>
2023-09-22 14:25:39 +0200

    purge user_tokens table

--- lib/mix/tasks/comiditas.purge.ex
+++ lib/mix/tasks/comiditas.purge.ex
@@ -2,16 +2,19 @@ defmodule Mix.Tasks.Comiditas.Purge do
   use Mix.Task
 
   import Ecto.Query
+  alias Comiditas.Accounts.UserToken
   alias Comiditas.{Frozen, Mealdate}
   alias Comiditas.Repo
 
   @shortdoc "Purge mealdate table of old data."
   def run(_) do
     date = Timex.today() |> Timex.shift(months: -1)
+    date_time = DateTime.now!("Etc/UTC") |> DateTime.add(-60*60*24*30, :second)
 
     Ecto.Migrator.with_repo(Repo, fn repo ->
       repo.delete_all(from m in Mealdate, where: m.date < ^date)
       repo.delete_all(from f in Frozen, where: f.date < ^date)
+      repo.delete_all(from u in UserToken, where: u.inserted_at < ^date_time)
     end)
   end
 end
